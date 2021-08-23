f71f31a12bc3486c7ff40367dde2907fd0625384
Budi Darmawan <clearesta.hansa@gmail.com>
2021-08-23 21:00:18 +0700

    latihan methode lain

--- 100 Days of Code - The Complete Python Pro Bootcamp for 2021/excercise/palindrome.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2021/excercise/palindrome.py
@@ -1,20 +1,28 @@
 import unittest
 
-def palindrome(text):
-    test = []
-    result = []
-    for i in range(0,(len(text))):
-        test.append(text[i])
-    for i in range(1,(len(text)+1)):
-        result.append(text[-i])
-    for i in range(0,len(result)):
-        if test[i] == result[i]:
+#def palindrome(text):
+#    test = []
+#    result = []
+#    for i in range(0,(len(text))):
+#        test.append(text[i])
+#    for i in range(1,(len(text)+1)):
+#        result.append(text[-i])
+#    for i in range(0,len(result)):
+#        if test[i] == result[i]:
+#            return True
+#        else:
+#            return False
+        
+def palindrome (text):
+    for i in range(0, len(text)):
+        if text[i] == text[(len(text)-1)-i]:
             return True
         else:
             return False
 
+
 #print(palindrome("kodok"))
-class TestCuboid(unittest.TestCase):
+class Test(unittest.TestCase):
     def test_volume(self):
         self.assertEqual(palindrome("budi"), False)
         self.assertEqual(palindrome("kodok"), True)
